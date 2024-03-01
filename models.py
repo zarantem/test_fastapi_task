8e349aa1309f90c12d9738795797c45d3854aae5
zarantem <89732957+zarantem@users.noreply.github.com>
2024-03-01 16:10:19 +0500

    first_commit
    Описал базовую модель pydantic, установил все зависимости

---
+++ models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+from datetime import date, datetime
+
+class shift_task(BaseModel):
+    close_status: bool = Field(alias='СтатусЗакрытия')
+    shift_task_submission: str = Field(alias='ПредставлениеЗаданияНаСмену')
+    work_center: str = Field(alias='Рабочий центр')
+    shift: str = Field(alias='Смена')
+    brigade: str = Field(alias='Бригада')
+    partion_naumber: int = Field(alias='НомерПартии')
+    partion_date: date = Field(alias='ДатаПартии')
+    nomenclature: str = Field(alias='Номенклатура')
+    ekn_code: str = Field(alias='КодЕКН')
+    rc_id: str = Field(alias='ИдентификаторРЦ')
+    shift_starts_at: datetime = Field(alias='ДатаВремяНачалаСмены')
+    shift_ends_at: datetime = Field(alias='ДатаВремяОкончанияСмены')
+    closed_at: datetime = Field(alias='время закрытия')
